2748bd2d6f83a3adcbc61906e46e728e3484e101
LailaL88 <58104146+LailaL88@users.noreply.github.com>
2020-07-05 18:20:58 +0300

    Add files via upload

---
+++ precache-manifest.9362e82872801fc7e93a68582ec24c68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11aa14bddbc0de70f027c45bac8a0d74",
+    "url": "/checkslider/index.html"
+  },
+  {
+    "revision": "983b8753f333b5300127",
+    "url": "/checkslider/static/css/main.60fd27bd.chunk.css"
+  },
+  {
+    "revision": "6090c83908ee6745c657",
+    "url": "/checkslider/static/js/2.6475de71.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/checkslider/static/js/2.6475de71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "983b8753f333b5300127",
+    "url": "/checkslider/static/js/main.8498a21a.chunk.js"
+  },
+  {
+    "revision": "61a8a8fd586c1ba6bd9d",
+    "url": "/checkslider/static/js/runtime-main.bfc0c877.js"
+  }
+]);
